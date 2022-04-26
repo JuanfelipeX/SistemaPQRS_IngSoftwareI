432aeb6039310511fd55dc290a9d43b6e0260b39
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-04-26 11:40:03 -0500

    mirando y arreglando algunos errores del mostrado de api en postam y terminal

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/userController");
+const userModel = require("../model/user"); //aqui se usa para mostralo en postman
+const userController = require("../controllers/userController"); //aqui se usa para mostrarlo en la terminal de VSC
 
-/*
+
 router.get("/user", (req, res) => {
-    userController
+    userModel
         .find()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
-*/
 
-router.get("/user", userController)
+
+//router.get("/user", userController)
 
 module.exports = router;
